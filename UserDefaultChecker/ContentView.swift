5d25416043637750bb121b5fc48a28a0d8c21321
1ris_W <ediath462@gmail.com>
2025-02-23 05:02:41 +0800

    Initial Commit

---
+++ UserDefaultChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserDefaultChecker
+//
+//  Created by Iris on 2025-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserDefaultChecker/UserDefaultCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultCheckerApp.swift
+//  UserDefaultChecker
+//
+//  Created by Iris on 2025-02-23.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
